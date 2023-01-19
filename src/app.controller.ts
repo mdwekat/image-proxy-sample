9cd1d61b330659ef536142d4c5d938a338d6adb4
mustafa <mustafa@dwekat.com>
2023-01-19 17:49:50 +0400

    sample image proxy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import {
+  Controller,
+  Get,
+  Header,
+  InternalServerErrorException,
+  StreamableFile,
+} from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Header('Content-Type', 'image/jpeg')
+  async getHello(): Promise<StreamableFile> {
+    try {
+      const request = this.httpService.get(
+        'https://i.picsum.photos/id/701/200/300.jpg?hmac=gVWdD9Rh_J0iGXpXOJAN7MZpGPrpeHX_M5JwGGvTSsI',
+        {
+          // This part is very important
+          responseType: 'arraybuffer',
+        },
+      );
+      const res = await lastValueFrom(request);
+
+      return new StreamableFile(Buffer.from(res.data, 'binary'), {
+        type: 'image/jpeg',
+      });
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 }
